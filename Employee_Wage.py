708e30b6f1655776b0fa64edc79df1ef1e3063b0
nick96k <nickpatil876@gmail.com>
2024-08-22 17:36:40 +0530

    [Nikhil] refactor: Changes in function and main

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -8,7 +8,6 @@
 
 
 '''
-
 import random
 
 WAGE_PER_HOUR = 20
@@ -18,46 +17,35 @@ MONTH_DAYS = 20
 MAX_HOURS_PER_MONTH = 100
 
 
-
 def check_attendance():
-
     """
 
         Description:
             Using random to check employee is present or not. If 0 then employee is absent, 1 for present and 2 for part time. 
-
         Parameter:
             None
-
         Return:
             Give choice 0, 1 or 2.
 
     """
-
     return random.choice([0, 1, 2])  
 
 
 def calculate_daily_wage():
-
     """
 
         Description:
             Calculate Daily wages, wage per hour is 20 and full day hour is 8. 
-
         Parameter:
             None
-
         Return:
             Multiplication of wage per hour and full day hours.
 
     """
-
     return WAGE_PER_HOUR * FULL_DAY_HOUR
 
 
-
 def part_time_employee_daily_wage():
-
     """
 
         Description:
@@ -73,37 +61,30 @@ def part_time_employee_daily_wage():
     return PART_TIME_DAY_HOUR * WAGE_PER_HOUR 
 
 
-
 def calculate_monthly_wage():
-
     """
 
         Description:
             Calculate monthly wages for the employees, for maximum hours in month is 100 and maximum day is 20.
-
         Parameter:
             None
-
         Return:
             total_days : Give Total working days in a month.
             total_hours : Give Total working hours in a month.
 
-    """
-    
+    """ 
     total_hours = 0
     total_days = 0
 
     while total_hours < MAX_HOURS_PER_MONTH and total_days < MONTH_DAYS  :
         attendance = check_attendance()
         if attendance == 1:
-            total_days +=1
-            total_hours += 8
+            total_hours += FULL_DAY_HOUR
         elif attendance == 2:
-            total_days +=1
-            total_hours += 4
+            total_hours += PART_TIME_DAY_HOUR
         else :
-            total_days +=1
             total_hours += 0
+        total_days +=1
 
     return total_days, total_hours
 
@@ -118,5 +99,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
